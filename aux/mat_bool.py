61aea1716d8f2f8ca0a275d1d0e2caae21826008
Felipe Santos <ffss@cin.ufpe.br>
2022-09-27 06:04:56 -0300

    redimensionando a bitmap

--- aux/mat_bool.py
+++ aux/mat_bool.py
@@ -4,12 +4,15 @@ import matplotlib.pyplot as plt
 
 img = cv2.imread('mapa_colisao.png')
 
+img = cv2.resize(img, (800, 450)) 
 height, width = img.shape[:2]
+
 with open('mat.bitmap', 'w') as f:
     f.write(str(height));
     f.write(' ');
     f.write(str(width));
-    
+    f.write('\n')
+
     for i in range(height):
         for j in range(width):
             #print(img[i, j])
@@ -22,6 +25,7 @@ with open('mat.bitmap', 'w') as f:
         #print()
         f.write('\n')
 f.close()
+
 cv2.imshow('image',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
