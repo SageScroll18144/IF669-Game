45399469df1d189d8dcf8b9d5b90c5cfa4802752
Felipe Santos <ffss@cin.ufpe.br>
2022-09-27 20:33:11 -0300

    finalizando o teste

--- aux/leitura.c
+++ aux/leitura.c
@@ -1,18 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-
-    FILE *file = fopen("mat.txt", "r");
+void ler(char *file_name){
+    FILE *file = fopen(file_name, "r");
     int l[2];
 
-    if(file == NULL) exit(1);
-
+    if(file == NULL) {
+        printf("*ERRO DE ABERTURA*\n");
+        exit(1);
+    }
     fscanf(file, "%d %d", &l[0], &l[1]);
+
+    int mat[l[0]][l[1]];
 
     printf("%d %d\n", l[0], l[1]);
 
+    for(int i=0;i<l[0];i++) for(int j=0;j<l[1];j++) fscanf(file, "%d", &mat[i][j]);
+    // for(int i=0;i<l[0];i++) {
+    //     for(int j=0;j<l[1];j++){
+    //         printf("%d ", mat[i][j]);
+    //     } 
+    //     printf("\n");
+    // }
+    printf("dim(0, 0) -> %d\n", mat[0][0]);
     fclose(file);
+
+}
+
+int main(){
+
+    ler("mat.txt");
 
     return 0;
 }
