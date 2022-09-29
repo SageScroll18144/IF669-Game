ad488932a69bad55449f001d4f9d02f256f90ee2
Felipe Santos <ffss@cin.ufpe.br>
2022-09-29 10:33:09 -0300

    colisao do mapa funcionando

--- aux/mat_bool.py
+++ aux/mat_bool.py
@@ -2,12 +2,17 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-img = cv2.imread('mapa_rcol.png')
+img = cv2.imread('mapa_colisao.png')
 
-#img = cv2.resize(img, (800, 450)) 
+entrada = input("Do you want to you resize image[y/n]: ")
+
+if entrada == 'y':
+    img = cv2.resize(img, (800, 450)) 
 height, width = img.shape[:2]
-print(width)
+#print(width)
 #cv2.imwrite("mapa_rcol.png", img) 
+
+print("LOAD IMAGE...")
 
 with open('mat.txt', 'w') as f:
     f.write(str(width));
@@ -18,7 +23,7 @@ with open('mat.txt', 'w') as f:
     for i in range(450):
         for j in range(800):
             #print(img[i, j])
-            print(i)
+            #print(i)
             if((img[i, j]==[255,255,255]).all()):
                 #print(1, end=' ')
                 f.write('1 ');
