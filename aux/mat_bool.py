4033bd9f30801b59afd55e9f0bea9c23c53d59c3
Felipe Santos <ffss@cin.ufpe.br>
2022-09-29 08:09:31 -0300

    mapa gerador txt

--- aux/mat_bool.py
+++ aux/mat_bool.py
@@ -2,10 +2,12 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-img = cv2.imread('mapa_colisao.png')
+img = cv2.imread('mapa_rcol.png')
 
-img = cv2.resize(img, (800, 450)) 
+#img = cv2.resize(img, (800, 450)) 
 height, width = img.shape[:2]
+print(width)
+#cv2.imwrite("mapa_rcol.png", img) 
 
 with open('mat.txt', 'w') as f:
     f.write(str(width));
@@ -13,9 +15,10 @@ with open('mat.txt', 'w') as f:
     f.write(str(height));
     f.write('\n')
 
-    for i in range(height):
-        for j in range(width):
+    for i in range(450):
+        for j in range(800):
             #print(img[i, j])
+            print(i)
             if((img[i, j]==[255,255,255]).all()):
                 #print(1, end=' ')
                 f.write('1 ');
