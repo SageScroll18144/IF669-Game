63244b1784b3383a00d728f589b591b9135271c2
Mend25 <113634496+Mend25@users.noreply.github.com>
2022-10-03 10:24:06 -0300

    Update menu.c

--- main/menu/menu.c
+++ main/menu/menu.c
@@ -6,6 +6,7 @@
 char title[] = "TITULO DO JOGO";
 char msg1[] = "Press Enter to start";
 char msg2[] = "Press y for instructions";
+char msg3[] = "Press x to exit the game";
 
 Font font;
 Vector2 position;
@@ -26,6 +27,7 @@ void drawMenu(){
 
     DrawTextEx(font, title, position, font.baseSize*2.0f ,(float)spacings, RED);
     DrawText(msg1 , 280, 250, 20, LIGHTGRAY);
-    DrawText(msg2 , 400, 250, 20, LIGHTGRAY);
+    DrawText(msg2 , 280, 400, 20, LIGHTGRAY);
+    DrawText(msg3 , 280, 550, 20, LIGHTGRAY);
 }
 
