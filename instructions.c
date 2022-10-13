91fc44ff77c1dce073d4180df6f8f3b920f3477e
Mend25 <113634496+Mend25@users.noreply.github.com>
2022-10-13 01:59:21 -0300

    Create instructions.c
    Criando instruções

---
+++ instructions.c
@@ -0,0 +1,22 @@
+#include "raylib.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+Font fonte;
+Image image;
+Texture2D texture;
+
+void instructions() {
+  while(!IsKeyPressed(KEY_ENTER)) {
+     BeginDrawing(); 
+     fonte = LoadFont("fonts/jupiter_crash.png");
+     DrawTexture(textura, 0, 0, WHITE);
+     image = LoadImage("assets/Instructions.png");
+     texture = LoadTextureFromImage(imagem);
+    
+     EndDrawing();
+}
+CloseWindow();
+  }
+
+
