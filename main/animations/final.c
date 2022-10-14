e83f284492e9d4815fdc64282e030ff557520151
GuilhermeMontt <113855003+GuilhermeMontt@users.noreply.github.com>
2022-10-14 01:25:02 -0300

    Update final.c

--- main/animations/final.c
+++ main/animations/final.c
@@ -10,20 +10,20 @@ void transiton3Dto2D(){
         BeginDrawing();//
         if(contador_3d < 2500){
             DrawCircle(400, 225, raio_3d, BLACK);
-            raio_3d++;
+            raio_3d = raio_3d + 5;
         }
         
         else{
             DrawCircle(400, 225, raio_3d, BLACK);
-            raio_3d--;
+            raio_3d = raio_3d - 5;
         }
         
-        if(contador_3d > 1000 && contador_3d < 4000){
+        if(contador_3d > 800 && contador_3d < 4200){
             DrawText("apos gusttavo dos ponteiros largar o mundo da musica e derrotar uma leva de morcegos", 50, 120, 17, RED);
             DrawText("ele passa muito tempo se preparando para o confronto contra o ACM", 110, 160, 17, RED);
             DrawText("derrote o ACM e salve os alunos de IP do CIn!", 154, 200, 20, RED);
         }
-        contador_3d++;
+        contador_3d = contador_3d + 5;
         EndDrawing();//
     }
     return;
