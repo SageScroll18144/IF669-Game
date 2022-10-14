00d6a191523040c954b6a8ab3f46b3d38d1cec3e
Mend25 <113634496+Mend25@users.noreply.github.com>
2022-10-14 02:41:51 -0300

    Update zombie.c

--- main/enemy/zombie.c
+++ main/enemy/zombie.c
@@ -4,7 +4,7 @@
 #include "zombie.h"
 
 //Inicialização do zumbi
-Enemy enemyArr[17];
+Enemy enemyArr[13];
 int how_many; 
 int hit_kill;
 
@@ -15,7 +15,7 @@ int currentFramZ = 0;
 int sideOrientation = 1;
 //Função de inicialização
 void initEnemy(){
-    for(int i=0;i<17;i++) {
+    for(int i=0;i<13;i++) {
         enemyArr[i].axisOrientation = 0;
         enemyArr[i].position = (Vector2) {rand() % 800, rand() % 450};
 
@@ -28,7 +28,7 @@ void initEnemy(){
 
     hit_kill = 50;
 
-    how_many = 17;
+    how_many = 13;
 
 }
 
