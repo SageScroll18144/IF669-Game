842fca0baee5388704721881e7141eb0783ee77b
Felipe Santos <ffss@cin.ufpe.br>
2022-10-14 06:21:51 -0300

    correção de acentuação no txt

--- main/animations/final.c
+++ main/animations/final.c
@@ -61,9 +61,9 @@ void transiton3Dto2D(){
         }
         
         if(contador_3d > 800 && contador_3d < 4200){
-            DrawText("apos gusttavo dos ponteiros largar o mundo da musica e derrotar uma leva de morcegos", 50, 120, 17, RED);
-            DrawText("ele passa muito tempo se preparando para o confronto contra o ACM", 110, 160, 17, RED);
-            DrawText("derrote o ACM e salve os alunos de IP do CIn!", 154, 200, 20, RED);
+            DrawText("Após gusttavo dos ponteiros largar o mundo da música e derrotar uma leva de morcegos", 50, 120, 17, RED);
+            DrawText("Ele caiu em um vórtex para o confronto final!", 220, 160, 17, RED);
+            DrawText("Derrote o Peter ACM Griffin e salve os alunos de IP do CIn!", 114, 200, 20, RED);
         }
         contador_3d = contador_3d + 5;
         EndDrawing();//
