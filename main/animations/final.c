a156ef2b6eded98d08181f3ae7cba09ea1ee509c
Mend25 <113634496+Mend25@users.noreply.github.com>
2022-10-14 01:20:12 -0300

    Update final.c

--- main/animations/final.c
+++ main/animations/final.c
@@ -6,23 +6,19 @@ int contador_3d = 0;
 int raio_3d = 0;
 
 void transiton3Dto2D(){
-    while(contador_3d < 10000){
+    while(contador_3d < 5000){
         BeginDrawing();//
-        if(contador_3d < 5000){
+        if(contador_3d < 2500){
             DrawCircle(400, 225, raio_3d, BLACK);
-            if(contador_3d%2 == 0){
-                raio_3d++;
-            }
+            raio_3d++;
         }
         
         else{
             DrawCircle(400, 225, raio_3d, BLACK);
-            if(contador_3d%2 == 0){
-                raio_3d--;
-            }
+            raio_3d--;
         }
         
-        if(contador_3d > 1000 && contador_3d < 9000){
+        if(contador_3d > 1000 && contador_3d < 4000){
             DrawText("apos gusttavo dos ponteiros largar o mundo da musica e derrotar uma leva de morcegos", 50, 120, 17, RED);
             DrawText("ele passa muito tempo se preparando para o confronto contra o ACM", 110, 160, 17, RED);
             DrawText("derrote o ACM e salve os alunos de IP do CIn!", 154, 200, 20, RED);
