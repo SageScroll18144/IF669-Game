188b49a48bf08b7146ec2869a26e000e851381d8
thyagobs <114110392+thyagobs@users.noreply.github.com>
2022-10-14 01:41:51 -0300

    Update final.c

--- main/animations/final.c
+++ main/animations/final.c
@@ -4,7 +4,53 @@
 
 int contador_3d = 0;
 int raio_3d = 0;
+Vector2 position_;
+int circle_, contador_;
+Sound vicMusic;
 
+void initVicScreen (){
+    position_ = (Vector2) {400, 225};
+    circle_ = 0; 
+    contador_ = 0;
+    vicMusic = LoadSound ("sounds/VicMusic.mp3");
+    SetSoundVolume (vicMusic, 0.8f); 
+}
+void drawVicScreen (){
+        if (!IsSoundPlaying (vicMusic)) PlaySound (vicMusic);
+        DrawCircle(position_.x, position_.y, (circle_ + 10), RED);
+        DrawCircle(position_.x, position_.y, circle_, WHITE);
+        if(contador_ > 220){
+            DrawText("CONGRATS!", 282, 100, 40, RED);
+        }
+        if(contador_ > 300){
+                DrawText("YOU", 360, 150, 40, RED);
+        }
+        if(contador_ > 350){
+                DrawText("ARE", 360, 200, 40, RED);
+        }
+        if(contador_ > 400){
+                DrawText("A", 390, 250, 40, RED);
+        }
+        if(contador_ > 450){
+                DrawText("LEGEND", 320, 300, 40, RED);
+        }
+        if(contador_ > 650){
+                DrawCircle(position_.x, position_.y, (circle_ + 10), RED);
+                DrawCircle(position_.x, position_.y, circle_, WHITE);
+                DrawText("By", 370, 60, 40, RED);
+                DrawText("Felipe Santos", 302, 130, 30, RED);
+                DrawText("Gabriel Nogueira", 272, 180, 30, RED);
+                DrawText("Guilherme Montenegro", 232, 230, 30, RED);
+                DrawText("Thyago Soares", 282, 280, 30, RED);
+                DrawText("Victor Mendonça", 265, 330, 30, RED);
+                DrawText("Press F to back to menu", 10, 410, 20, RED);
+        }
+        
+        if(circle_ < 200){
+            circle_+=1;
+        }
+        contador_++;
+}
 void transiton3Dto2D(){
     while(contador_3d < 5000){
         BeginDrawing();//
