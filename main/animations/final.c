5e4ce93d66be6863f6bdca6d4ae0ba506c0dc9c2
Felipe Santos <ffss@cin.ufpe.br>
2022-10-14 00:27:24 -0300

    alteracoes de victor

--- main/animations/final.c
+++ main/animations/final.c
@@ -6,9 +6,9 @@ int contador_3d = 0;
 int raio_3d = 0;
 
 void transiton3Dto2D(){
-    while(contador_3d <30000){
+    while(contador_3d < 3000){
         
-        if(contador_3d < 15000){
+        if(contador_3d < 1500){
             DrawCircle(400, 225, raio_3d, BLACK);
             if(contador_3d%2 == 0){
                 raio_3d++;
@@ -22,12 +22,13 @@ void transiton3Dto2D(){
             }
         }
         
-        if(contador_3d > 1000 && contador_3d < 29000){
+        if(contador_3d > 1000 && contador_3d < 2900){
             DrawText("apos gusttavo dos ponteiros largar o mundo da musica e derrotar uma leva de morcegos", 50, 120, 17, RED);
             DrawText("ele passa muito tempo se preparando para o confronto contra o ACM", 110, 160, 17, RED);
             DrawText("derrote o ACM e salve os alunos de IP do CIn!", 154, 200, 20, RED);
         }
         contador_3d++;
+        if(contador_3d == 2999) contador_3d = 1001;
     }
     return;
 }
